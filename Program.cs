9709e4dec56796c1cf59686b1068127856d621fa
Sandra Sovinska <90636952+SandraSovinska@users.noreply.github.com>
2021-09-15 20:23:29 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace NamuDarbai
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Console.WriteLine("koks jusu vardas");
+
+            var vardas = Console.ReadLine();
+
+            Console.WriteLine("kokia jusu pavarde?");
+
+            var pavarde = Console.ReadLine();
+
+            Console.WriteLine("kokia Jusu gimtine?");
+
+            var gimimoVieta = Console.ReadLine();
+
+            Console.Write( vardas +  pavarde +  gimimoVieta ) ;
+        }
+    }
+}
